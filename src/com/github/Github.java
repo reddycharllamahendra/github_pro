ad49d0d1878aeeb79fc43eebe81aa31877be24c9
Mahendra2414 <Mahendra2414@Mahendra>
2026-03-10 10:37:58 +0530

    git method updated

--- src/com/github/Github.java
+++ src/com/github/Github.java
@@ -2,7 +2,7 @@ package com.github;
 
 public class Github {
 	static void git() {
-		System.out.println("github tuts");
+		System.out.println("github tutorial");
 	}
 	public static void main(String[] args) {
 		Github.git();
